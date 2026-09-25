7bdd465e06d1fd33618da72f060b109b654d4da1
mojtabarozegar11-ship-it <mojtaba.rozegar11@gmail.com>
2026-09-25 09:05:30 +0330

    Route production home page

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-from django.urls import include,path
-urlpatterns=[path("admin/",admin.site.urls),path("api/",include("core.urls"))]
+from django.urls import include, path
+
+from core.views import home
+
+urlpatterns = [
+    path("", home, name="home"),
+    path("admin/", admin.site.urls),
+    path("api/", include("core.urls")),
+]
